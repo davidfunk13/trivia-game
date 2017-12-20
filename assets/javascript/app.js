b8d20508a2842199b34c123727d809fa76cfde3b
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-19 16:19:00 -0800

    cant console log variables...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-    var questions = [];
-    var allAnswers = [[][][][][][][][]];
-    var correctAnswers = [];
+
+    var questionsData = ['How much wood would a woodchuck chuck if a woodchuck could chuck wood?'];
+    var allAnswers = [[5,7,9,10000][][][][][][][]];
+    var correctAnswers = [7];
+    
+    $('#questions').html(questionsData);
+    console.log(questionsData);
 });
