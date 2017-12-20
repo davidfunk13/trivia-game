105f56d664ab3135480a6234720dbd17b1089e83
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-19 16:59:48 -0800

    jquery added, writing a questions array to the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
     var questionsData = ['How much wood would a woodchuck chuck if a woodchuck could chuck wood?'];
-    var allAnswers = [[5,7,9,10000][][][][][][][]];
+    var allAnswers = [5,7,9,10000];
     var correctAnswers = [7];
-    
+
     $('#questions').html(questionsData);
-    console.log(questionsData);
+    console.log(questionsData, allAnswers, correctAnswers);
 });
