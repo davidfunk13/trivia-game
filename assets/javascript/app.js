1be29609e323dfd10b65e44f5077f27e454832f8
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-21 14:01:14 -0800

    seeing a lot of ideas structuring the questions this way. readjusting.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,52 +2,28 @@ $(document).ready(function() {
 
 var questionOne = {
     question: "What is the answer to QuestionOne?",
-    answers: {
-        a: "seven",
-        b: "six",
-        c: "fourteen",
-        d: "twenty",
-        answer: "seven",
-    }
+    possibleAnswers = ["seven", "six", "fourteen", "twenty"],
+    correct: 0,
+    
 };
 
 var questionTwo = {
     question: "What is the answer to QuestionTwo?",
-    answers: {
-        a: "seven",
-        b: "six",
-        c: "fourteen",
-        d: "twenty",
-        answer: "seven",
-    }
+        possibleAnswers = ["seven", "six", "fourteen", "twenty"],
+        correct: 0,
 };
 var questionThree = {
     question: "What is the answer to QuestionThree?",
-    answers: {
-        a: "seven",
-        b: "six",
-        c: "fourteen",
-        d: "twenty",
-        answer: "seven",
-    }
+        possibleAnswers = ["seven", "six", "fourteen", "twenty"],
+        correct: 0,
 };
 var questionFour = {
     question: "What is the answer to questionFour?",
-    answers: {
-        a: "seven",
-        b: "six",
-        c: "fourteen",
-        d: "twenty",
-        answer: "seven",
-    }
+        possibleAnswers = ["seven", "six", "fourteen", "twenty"],
+        correct: 0,
 };
-console.log(questionOne.answers.a)
 var questionArray = [questionOne.question, questionTwo.question, questionThree.question, questionFour.question] 
-// console.log(questionArray[2].toString());
 $("#questionsdiv").html(questionOne.question);
-$("#answerone").html(questionOne.answers.a);
-$('#answertwo').html(questionOne.answers.b);
-$('#answerthree').html(questionOne.answers.c);
-$('#answerfour').html(questionOne.answers.d);
+
 });
  
