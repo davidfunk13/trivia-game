6541aecc6b09ba4cfcfb21cf33391622d9c326b6
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-21 15:48:13 -0800

    little but of progress, refined triviquestions into a single object to refernce with index. figuring out how to loop through array to print ansers to buttons.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var questionOne = {
-    question: "What is the answer to QuestionOne?",
-    possibleAnswers = ["seven", "six", "fourteen", "twenty"],
-    correct: 0,
-    
-};
+    var rightAnswers = 0;
+    var wrongAnswers = 0;
+    var unAnswered = 0;
 
-var questionTwo = {
-    question: "What is the answer to QuestionTwo?",
-        possibleAnswers = ["seven", "six", "fourteen", "twenty"],
-        correct: 0,
-};
-var questionThree = {
-    question: "What is the answer to QuestionThree?",
-        possibleAnswers = ["seven", "six", "fourteen", "twenty"],
-        correct: 0,
-};
-var questionFour = {
-    question: "What is the answer to questionFour?",
-        possibleAnswers = ["seven", "six", "fourteen", "twenty"],
-        correct: 0,
-};
-var questionArray = [questionOne.question, questionTwo.question, questionThree.question, questionFour.question] 
-$("#questionsdiv").html(questionOne.question);
+    var triviaQuestions = [{
+            question: "What is the answer to QuestionOne?",
+            possibleAnswers: ["seven", "six", "fourteen", "twenty"],
+            correct: 0,
+
+        },
+        {
+            question: "What is the answer to QuestionTwo?",
+            possibleAnswers: ["seven", "six", "fourteen", "twenty"],
+            correct: 0,
+        },
+        {
+            question: "What is the answer to QuestionThree?",
+            possibleAnswers: ["seven", "six", "fourteen", "twenty"],
+            correct: 0,
+        },
+        {
+            question: "What is the answer to questionFour?",
+            possibleAnswers: ["seven", "six", "fourteen", "twenty"],
+            correct: 0,
+        }
+    ];
+    console.log(triviaQuestions[0].question);
+    console.log(triviaQuestions[0].possibleAnswers)
+    //question one work
+    $('#questionsdiv').html(triviaQuestions[0].question);
+    $('#answerone').html(triviaQuestions[0].possibleAnswers[0]);
+    $('#answertwo').html(triviaQuestions[0].possibleAnswers[1]);
+    $('#answerthree').html(triviaQuestions[0].possibleAnswers[2]);
+    $('#answerfour').html(triviaQuestions[0].possibleAnswers[3]);
+
+    //question one control flow
+correctAnswerCheck()
+
 
 });
- 
