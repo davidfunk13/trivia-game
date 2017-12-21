7c463369bfbf075c1b773df3f6da807965f1d6d7
Dave <davefunk135@gmail.com>
2017-12-20 22:03:06 -0800

    html changes. working things out in js, function for randomizing questions, function for starting the game coming next

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,44 @@ var questionOne = {
         d: "twenty",
         answer: "seven",
     }
+};
+
+var questionTwo = {
+    question: "Why",
+    answers: {
+        a: "seven",
+        b: "six",
+        c: "fourteen",
+        d: "twenty",
+        answer: "seven",
+    }
+};
+var questionThree = {
+    question: "How many times is too many",
+    answers: {
+        a: "seven",
+        b: "six",
+        c: "fourteen",
+        d: "twenty",
+        answer: "seven",
+    }
+};
+var questionFour = {
+    question: "Who dunnit",
+    answers: {
+        a: "seven",
+        b: "six",
+        c: "fourteen",
+        d: "twenty",
+        answer: "seven",
+    }
+};
+function randomQuestion() {
+    var questionArray = [questionOne.question, questionTwo.question, questionThree.question, questionFour.question]
+
+
 }
-console.log(questionOne.answers.a);
+$("#questionsdiv").html(questionOne.question);
+
+console.log(questionOne.question);;
 });
